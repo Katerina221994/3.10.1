3802a48d848575d53da2268d29774022779ff856
Ekaterina <rfvb.foto@mail.ru>
2024-01-27 10:11:32 +0200

    Добавьте файлы проекта.

---
+++ Четное или нечетное/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Четное_или_нечетное
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            int number = int.Parse(Console.ReadLine());
+            
+
+            if(number%2 == 0)
+            {
+                Console.WriteLine("Число четное");
+                
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+                
+            }
+            Console.ReadKey();
+        }
+        
+        
+    }
+    
+}
